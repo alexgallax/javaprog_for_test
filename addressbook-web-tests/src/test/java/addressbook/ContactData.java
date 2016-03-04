16a199eda1f0326027b06ab23fcb19093aa6e26e
alexgallax <fort21k@mail.ru>
2016-03-04 23:44:13 +0300

    add contact tests class with parametrized methods, class for contact data

---
+++ addressbook-web-tests/src/test/java/addressbook/ContactData.java
@@ -0,0 +1,49 @@
+package addressbook;
+
+public class ContactData {
+    private final String firstName;
+    private final String middleName;
+    private final String lastName;
+    private final String address;
+    private final String mobile;
+    private final String email;
+    private final String notes;
+
+    public ContactData(String firstName, String middleName, String lastName, String address, String mobile, String email, String notes) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.address = address;
+        this.mobile = mobile;
+        this.email = email;
+        this.notes = notes;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+}
